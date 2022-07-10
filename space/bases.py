715d02fe62ded24df1e4da25328400d9773c96b4
Kalleo Ouriques <kalleoouriques@pop-os.localdomain>
2022-07-10 17:49:39 -0300

    Método de abastecimento de foguetes e das bases(tirando a lua) implementado

--- space/bases.py
+++ space/bases.py
@@ -28,46 +28,90 @@ class SpaceBase(Thread):
     def base_rocket_resources(self, rocket_name):
         match rocket_name:
             case 'DRAGON':
-                if self.uranium > 35 and self.fuel > 50:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA':
-                        self.fuel = self.fuel - 70
-                    elif self.name == 'MOON':
-                        self.fuel = self.fuel - 50
-                    else:
-                        self.fuel = self.fuel - 100
+                quant_oil = {'ALCANTARA': 70, 'MOON': 50, 'MOSCOU': 100, 'CABO CANAVERAL': 100}
+
             case 'FALCON':
-                if self.uranium > 35 and self.fuel > 90:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA':
-                        self.fuel = self.fuel - 100
-                    elif self.name == 'MOON':
-                        self.fuel = self.fuel - 90
-                    else:
-                        self.fuel = self.fuel - 120
+                quant_oil = {'ALCANTARA': 100, 'MOON': 90, 'MOSCOU': 120, 'CABO CANAVERAL': 120}
+               
             case 'LION':
-                if self.uranium > 35 and self.fuel > 100:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA':
-                        self.fuel = self.fuel - 100
-                    else:
-                        self.fuel = self.fuel - 115
+                quant_oil = {'ALCANTARA': 100, 'MOON': 'OUT', 'MOSCOU': 115, 'CABO CANAVERAL': 115}
+        
             case _:
                 print("Invalid rocket name")
+                return False
+        
+        if (quant_oil[self.name] == 'OUT'):
+            print("IMPOSSÍVEL LANÇAR O FOGUETE LION DA LUA!")
+            return False
+        
+        abastecido = False
+        uranium_ok =  False
+        oil_ok = False
+      
+        while(not(abastecido)):
 
+            # verifica se tem recurso suficiente
+            if self.uranium >= 35:
+                uranium_ok = True
+                self.uranium = self.uranium - 35
+            
+            if (self.fuel >= quant_oil[self.name]):
+                oil_ok = True
+                self.fuel -= quant_oil[self.name]
+
+            # não tem recurso suficiente pra abastecer o foguete
+            if (not(uranium_ok) or not(oil_ok)):
+                # tenta reabastecer a base
+                # se conseguir o loop é refeito
+                abastecido = self.tenta_reabastecer_base(35, quant_oil[self.name])
+            
+            # tem recurso, então o foguete já foi abastecido
+            # e o loop não deve continuar
+            else:
+                abastecido = True
+    
+    def tenta_reabastecer_base(self, q_uranium, q_fuel):
+        # não tem recurso suficiente pra lançar o foguete
+        uranium_ok = False
+        fuel_ok = False
+        # verifica se o recurso faltante é uranium
+        # e tenta abastecer
+        if self.uranium < q_uranium:
+            self.refuel_uranium()
+            if self.uranium < q_uranium:
+                uranium_ok = False
+            
+            else:
+                uranium_ok = True
+        else:
+            uranium_ok = True
+
+        # verifica se o recurso faltante é oil
+        # e tenta abastecer
+        if self.fuel < q_fuel:
+            self.refuel_oil()
+            if self.fuel < q_fuel:
+                fuel_ok = False
+
+            else:
+                fuel_ok = True
+        else:
+            fuel_ok = True
+
+        # retorna a condição de abastecimento ou não
+        # a not aqui, ocorre pelo fato de o loop no método
+        # que chama, aconetecer no caso da variável que recebe
+        # ser falsa
+        return (not(uranium_ok and fuel_ok))    
 
     def refuel_oil(self):
 
         oil = globals.mines['oil_earth']
-        
-        # Vemos qual quantidade tem que ser verificada dependendo da base
-        if self.name == 'CANAVERAL CAPE' or self.name == 'MOSCOW':
-            unidades_oil = 120
-        elif self.name == 'ALCANTARA':
-            unidades_oil = 100
-        
-        # Verificamos se precisa repor o combustivel
-        if self.oil < unidades_oil:
+
+        # verifica se é a lua que quer ser reabastecida
+        if self.name == 'MOON':
+            pass
+        else:
             # Mutex para o acesso a mina de combustivel
             with globals.lock_oil:
                 oil_atual = oil.unities
@@ -82,8 +126,10 @@ class SpaceBase(Thread):
 
         mina = globals.mines['uranium_earth']
 
-        # Verificamos se é preciso repor o uranio
-        if self.uranium < 35:
+        # verifica se é a lua que quer ser reabastecida
+        if self.name == 'MOON':
+            pass
+        else:
             # Mutex para o acesso a mina de uranio
             with globals.lock_uranio:
                 mina_atual = mina.unities
